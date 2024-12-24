3e783421a67204a69cd9fe823174251f89e49f15
Poberezhnyuk Nikita <katshido.2003@gmail.com>
2024-12-24 19:10:48 +0300

    final fix

--- src/test/java/mirea/ru/reactive/controller/ComputerControllerTests.java
+++ src/test/java/mirea/ru/reactive/controller/ComputerControllerTests.java
@@ -3,6 +3,7 @@ package mirea.ru.reactive.controller;
 import mirea.ru.reactive.entity.Computer;
 import mirea.ru.reactive.generated.ComputerData;
 import mirea.ru.reactive.repository.ComputerRepository;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +18,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static reactor.core.publisher.Mono.when;
 
-@ExtendWith(MockitoExtension.class)
 public class ComputerControllerTests {
 
     @BeforeEach
@@ -30,8 +30,7 @@ public class ComputerControllerTests {
         Computer computer = ComputerData.getComputer1();
 
         ComputerRepository computerRepository = Mockito.mock(ComputerRepository.class);
-        when(computerRepository.findById(1L)).thenReturn(Mono.just(computer));
-
+        Mockito.when(computerRepository.findById(1L)).thenReturn(Mono.just(computer));
         ComputerController computerController = new ComputerController(computerRepository);
         ResponseEntity<Computer> response = computerController.getComputerById(1L).block();
 
@@ -46,11 +45,52 @@ public class ComputerControllerTests {
         Computer computer2 = ComputerData.getComputer2();
 
         ComputerRepository computerRepository = Mockito.mock(ComputerRepository.class);
-        when(computerRepository.findAll()).thenReturn(Flux.just(computer1, computer2));
+        Mockito.when(computerRepository.findAll()).thenReturn(Flux.just(computer1, computer2));
         ComputerController computerController = new ComputerController(computerRepository);
-
         Flux<Computer> response = computerController.getAllComputers(null);
 
         assertEquals(2, response.collectList().block().size());
+    }
+
+    @Test
+    public void createComputer_givenComputer_savesComputer() {
+        Computer computer = ComputerData.getComputer1();
+
+        ComputerRepository computerRepository = Mockito.mock(ComputerRepository.class);
+        Mockito.when(computerRepository.save(computer)).thenReturn(Mono.just(computer));
+        ComputerController computerController = new ComputerController(computerRepository);
+        Mono<Computer> response = computerController.createComputer(computer);
+
+        assertEquals(computer, response.block());
+    }
+
+    @Test
+    public void updateComputer_givenComputer_updatesComputer() {
+        Computer computer = ComputerData.getComputer1();
+        Computer computerUpdated = new Computer(computer.getId(), "new_cpu", "new_gpu", computer.getRam(), computer.getStorage());
+
+        ComputerRepository computerRepository = Mockito.mock(ComputerRepository.class);
+        Mockito.when(computerRepository.findById(1L)).thenReturn(Mono.just(computer));
+        Mockito.when(computerRepository.save(computer)).thenReturn(Mono.just(computerUpdated));
+        ComputerController computerController = new ComputerController(computerRepository);
+        ResponseEntity<Computer> response = computerController.updateComputer(1L, computerUpdated).block();
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(computerUpdated, response.getBody());
+    }
+
+    @Test
+    public void deleteComputer_givenId_deletesComputer() {
+        Computer computer = ComputerData.getComputer1();
+
+        ComputerRepository computerRepository = Mockito.mock(ComputerRepository.class);
+        Mockito.when(computerRepository.findById(1L)).thenReturn(Mono.just(computer));
+        Mockito.when(computerRepository.delete(computer)).thenReturn(Mono.empty());
+        ComputerController computerController = new ComputerController(computerRepository);
+        ResponseEntity<Void> response = computerController.deleteComputer(1L).block();
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 }
